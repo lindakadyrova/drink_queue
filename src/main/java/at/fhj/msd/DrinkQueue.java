1cd3265fd42394734f612c0c4b3628a5b31bc064
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2025-04-26 18:43:39 +0200

    Add last three methods of DrinkQueue

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,5 +1,28 @@
 package at.fhj.msd;
-
+ 
+import java.util.ArrayList;
+import java.util.List; 
+ 
 public class DrinkQueue {
-
+    private List<Drink> elements = new ArrayList<>();
+    private final int maxSize;
+ 
+    public DrinkQueue(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("Maximum size must be > 0");
+        }
+        this.maxSize = maxSize;
+    }
+ 
+    public int size() {
+        return elements.size();
+    }
+ 
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+ 
+    public boolean isFull() {
+        return elements.size() >= maxSize;
+    }
 }
