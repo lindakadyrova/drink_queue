55c4df46a117ed4dff64f5c5d5be26fad1cb9e6b
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2025-04-26 19:07:43 +0200

    Add javadoc comments

--- src/main/java/at/fhj/msd/Cocktail.java
+++ src/main/java/at/fhj/msd/Cocktail.java
@@ -2,15 +2,29 @@ package at.fhj.msd;
 
 import java.util.List;
 
+/**
+ * Represents a Cocktail which is a mix of multiple Liquids.
+ */
 public class Cocktail extends Drink {
 
   private List<Liquid> ingredients;
 
+  /**
+   * Creates a new Cocktail with a given name and a list of ingredients.
+   *
+   * @param name the name of the cocktail
+   * @param ingredients the list of liquids used in the cocktail
+   */
   public Cocktail(String name, List<Liquid> ingredients) {
     super(name);
     this.ingredients = ingredients;
   }
 
+  /**
+   * Calculates the total volume of the cocktail by summing the volumes of all ingredients.
+   *
+   * @return the total volume of the cocktail
+   */
   @Override
   public double getVolume() {
     return ingredients.stream()
@@ -18,6 +32,12 @@ public class Cocktail extends Drink {
         .sum();
   }
 
+  /**
+   * Calculates the alcohol percentage of the cocktail based on its ingredients.
+   * If the total volume is zero, returns 0.0.
+   *
+   * @return the alcohol percentage of the cocktail
+   */
   @Override
   public double getAlcoholPercent() {
     double totalVolume = getVolume();
@@ -31,16 +51,31 @@ public class Cocktail extends Drink {
     return (alcoholAmount / totalVolume) * 100.0;
   }
 
+  /**
+   * Checks if the cocktail contains any alcoholic ingredients.
+   *
+   * @return true if the cocktail is alcoholic, false otherwise
+   */
   @Override
   public boolean isAlcoholic() {
     return ingredients.stream()
         .anyMatch(liquid -> liquid.getAlcoholPercent() > 0.0);
   }
 
+  /**
+   * Returns the list of ingredients used in the cocktail.
+   *
+   * @return the list of ingredients
+   */
   public List<Liquid> getIngredients() {
     return ingredients;
   }
 
+  /**
+   * Sets the list of ingredients for the cocktail.
+   *
+   * @param ingredients the new list of liquids
+   */
   public void setIngredients(List<Liquid> ingredients) {
     this.ingredients = ingredients;
   }
--- src/main/java/at/fhj/msd/Drink.java
+++ src/main/java/at/fhj/msd/Drink.java
@@ -1,46 +1,54 @@
 package at.fhj.msd;
 
 /**
- * Abstract base class for concrete drink classes
+ * Abstract base class for concrete drink classes.
+ * This class defines the structure and essential methods for any drink.
  */
 public abstract class Drink {
     /**
-     * name of the drink
+     * The name of the drink.
      */
     protected String name;
     /**
-     * Creates a Drink object with given name
+     * Creates a Drink object with the given name.
      *
-     * @param name name of the drink
-     *
+     * @param name the name of the drink
      */
     public Drink(String name) {
         this.name = name;
     }
     /**
-     * Calculates and returns volume of drink
+     * Calculates and returns the volume of the drink.
      *
-     * @return the volume of drink in liter
+     * @return the volume of the drink in liters
      */
     public abstract double getVolume();
     /**
-     * Calculates and returns the alcohol percentage
+     * Calculates and returns the alcohol percentage of the drink.
      *
-     * @return alcohol volume percent (e.g. 50)
+     * @return the alcohol volume percentage (e.g., 50)
      */
     public abstract double getAlcoholPercent();
     /**
-     * Gives information if drink is alcoholic or not
+     * Gives information on whether the drink is alcoholic or not.
      *
-     * @return true when alcoholic liquids are present, otherwise false
+     * @return true if the drink contains alcoholic liquids, otherwise false
      */
     public abstract boolean isAlcoholic();
 
-
+    /**
+     * Returns the name of the drink.
+     *
+     * @return the name of the drink
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Sets a new name for the drink.
+     *
+     * @param name the new name of the drink
+     */
     public void setName(String name) {
         this.name = name;
     }
